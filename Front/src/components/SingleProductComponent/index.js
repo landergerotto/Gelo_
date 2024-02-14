d2667597d2f1c48ee3607cbda5c6840d3d829cba
Lander Furusho Gerotto <lander.gerotto@gmail.com>
2024-02-14 11:57:41 -0300

    YO

--- Front/src/components/SingleProductComponent/index.js
+++ Front/src/components/SingleProductComponent/index.js
@@ -22,6 +22,21 @@ export default function SingleProductComponent() {
     setProduct(res.data);
   }
 
+  function Options()
+  {
+    // if(!product == {})
+    //   return
+    // if (product.color.length > 0)
+    // {
+    //   Array.from(product.color, (color) => {
+    //     return(
+    //       <option value={color}>{color}</option>
+    //     )
+    //   })
+
+    // }
+  }
+
   console.log(slug)
   return (
     <>
@@ -96,6 +111,8 @@ export default function SingleProductComponent() {
                     <div>
                       <Form.Select aria-label="Default select example">
                         <option disabled>Open this select menu</option>
+                        <Options></Options>
+
                         <option value="1">One</option>
                         <option value="2">Two</option>
                         <option value="3">Three</option>
